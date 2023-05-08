d9c02960dc44a265d83d1401d19011f9af4121a5
Salah Zanabili <Zanabilis@gmail.com>
2023-05-08 17:21:31 -0500

    tried to improve image loading

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images:{
-    domains:['static.wikia.nocookie.net']
-  }
 }
 
 module.exports = nextConfig
